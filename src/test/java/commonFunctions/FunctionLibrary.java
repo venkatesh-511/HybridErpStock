ad35b8c3ff7e6ff555c762b2adf7f93c45d4a7c9
srinu <srinu@DESKTOP-IA0U1JD>
2024-10-16 23:53:15 +0530

    Writthen Method and Excel oct16

--- src/test/java/commonFunctions/FunctionLibrary.java
+++ src/test/java/commonFunctions/FunctionLibrary.java
@@ -1,5 +1,159 @@
 package commonFunctions;
 
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+
 public class FunctionLibrary {
+	public static WebDriver driver;
+	public static Properties conpro;
+	// method for launching browser
+	public static WebDriver startBrowser() throws Throwable
+	{
+		conpro = new Properties();
+		conpro.load(new FileInputStream("PropertyFiles\\Environment.properties"));
+		if(conpro.getProperty("Browser").equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(conpro.getProperty("Browser").equalsIgnoreCase("firefox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else
+		{
+			Reporter.log("Browser value is not Matching",true);
+		}
+		return driver;
+		
+	}
+	// method for launching url
+	public static void openUrl() 
+	{
+		driver.get(conpro.getProperty("Url"));
+	} 
+	// method for wait to any element
+	public static void waitForElement(String LocatorType,String LocatorValue, String TestData) 
+	{
+		WebDriverWait mywait = new  WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(TestData)));
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			// wait until element is visible
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+	}
+	// method for any textbox
+	public static void typeAction(String LocatorType,String LocatorValue, String TestData)
+	{
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+	}
+	// method for buttons,checkbox,radiobutton,images,links
+	public static void clickAction(String LocatorType,String LocatorValue) 
+	{
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(Keys.ENTER);
+		}
+		
+	}
+	// method for validate page title
+	public static void validateTitle(String Expected_Title) 
+	{
+		String Actual_Title = driver.getTitle();
+		try 
+		{
+			Assert.assertEquals(Actual_Title, Expected_Title,"Tile is not Matching");
+		} catch (AssertionError a) {
+			System.out.println(a.getMessage());
+		}
+		
+	}
+	// method for close browser
+	public static void closeBrowser() 
+	{
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
